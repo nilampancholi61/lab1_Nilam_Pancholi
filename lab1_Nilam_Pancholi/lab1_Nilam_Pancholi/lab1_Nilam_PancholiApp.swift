c82dc904f876f9df412d000e89979fba83fd6266
nilampancholi61 <155118869+nilampancholi61@users.noreply.github.com>
2025-02-12 21:01:41 -0500

    adding

---
+++ lab1_Nilam_Pancholi/lab1_Nilam_Pancholi/lab1_Nilam_PancholiApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab1_Nilam_PancholiApp.swift
+//  lab1_Nilam_Pancholi
+//
+//  Created by Ankit Pancholi on 2025-02-11.
+//
+
+import SwiftUI
+
+@main
+struct lab1_Nilam_PancholiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lab1_Nilam_Pancholi/lab1_Nilam_PancholiTests/lab1_Nilam_PancholiTests.swift
@@ -0,0 +1,17 @@
+//
+//  lab1_Nilam_PancholiTests.swift
+//  lab1_Nilam_PancholiTests
+//
+//  Created by Ankit Pancholi on 2025-02-11.
+//
+
+import Testing
+@testable import lab1_Nilam_Pancholi
+
+struct lab1_Nilam_PancholiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
